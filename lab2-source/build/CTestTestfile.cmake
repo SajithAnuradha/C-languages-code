1b80bed1be363c66716f1e754228d0277dce1c54
Sajith <sajithanuradha860@gmail.com>
2024-01-18 10:13:19 +0530

    Finished the labs

--- lab2-source/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ASUS/Desktop/Module/Data Communication and Networks/Labs/lab2/lab2-source
-# Build directory: C:/Users/ASUS/Desktop/Module/Data Communication and Networks/Labs/lab2/lab2-source/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
